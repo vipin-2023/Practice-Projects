6cf05fd209abb79b32b99b0c2bf815e085e86433
vipin-2023 <githublogin321@gmail.com>
2023-01-29 15:07:14 +0530

    animated input

---
+++ animated_search_box/animated_loader/script.js
@@ -0,0 +1,27 @@
+const searchBtn = document.querySelector(".search-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const searchBox = document.querySelector(".search-box");
+const searchInput = document.querySelector("input");
+const searchData = document.querySelector(".search-data");
+
+searchBtn.onclick = () => {
+  searchBox.classList.add("active");
+  searchInput.classList.add("active");
+  searchBtn.classList.add("active");
+  cancelBtn.classList.add("active");
+  if (searchInput.value != "") {
+
+    searchData.classList.remove("active");
+   
+    searchData.innerHTML = "You just typed " + "<span style='font-weight:500'>" + searchInput.value + "</span>";
+  } else {
+    searchData.innerHTML = "";
+  }
+};
+cancelBtn.onclick = () => {
+  searchBox.classList.remove("active");
+  searchInput.classList.remove("active");
+  searchBtn.classList.remove("active");
+  cancelBtn.classList.remove("active");
+  searchData.classList.add("active");
+};
